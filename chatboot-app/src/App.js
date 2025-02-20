779f9e28a5c356ced676c2b7a5c9c5feaab640ea
startupsolellc <125030964+startupsolellc@users.noreply.github.com>
2025-02-20 13:43:39 +0000

    React projesi ilk kurulumu ve basit içerik eklendi

---
+++ chatboot-app/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Merhaba Chatboot! 🚀</h1>
+      <p>Bu, Startupsole.com için özel olarak hazırlanan bir chatboot uygulamasıdır.</p>
+    </div>
+  );
+}
+
+export default App;
