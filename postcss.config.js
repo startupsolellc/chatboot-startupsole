f015ef92c8f9cc6172a0228af2f797a019077c70
startupsolellc <125030964+startupsolellc@users.noreply.github.com>
2025-02-20 16:49:18 +0000

    PostCSS ve Tailwind CSS yapılandırması güncellendi

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 // postcss.config.js
 
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('@tailwindcss/postcss'),
-  ],
-};
+    plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+    ],
+  };
+  
