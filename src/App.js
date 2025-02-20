cf01699dfcae17d67dc5986ef78b1364c46d3b0c
startupsolellc <125030964+startupsolellc@users.noreply.github.com>
2025-02-20 17:28:24 +0000

    ChatWidget bileşeni basitleştirildi ve render testi yapılıyor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // src/App.js
 
 import React from 'react';
+import ChatWidget from './components/ChatWidget';
 
 function App() {
   return (
     <div>
       <h1>Merhaba Chatboot! 🚀</h1>
+      <ChatWidget />
     </div>
   );
 }
